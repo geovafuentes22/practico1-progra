cec528894ab30f27f291204f3801a1a04b9ece20
geovafuentes22 <geofuentes.gf@gmail.com>
2021-02-23 10:30:12 -0600

    Subiendo Ejercicio1

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double precio1, cantidad, proceso;
+
+            Console.WriteLine("Ingrese el precio del articulo");
+            precio1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese la cantidad de articulos que lleva");
+            cantidad = Double.Parse(Console.ReadLine());
+
+            proceso = cantidad * precio1;
+
+            Console.Write("El total a pagar es: $" + proceso);
+            Console.ReadLine();
+        }
+    }
+}
